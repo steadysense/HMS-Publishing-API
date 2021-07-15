d572ffd5a32536f066c1b70bb863cd7ad2307025
Jakob Maierhofer <jakob.maierhofer@steadysense.at>
2021-07-15 14:43:27 +0200

    make cmd-line rdy

--- examples/uploadApk.py
+++ examples/uploadApk.py
@@ -1,14 +1,15 @@
 # Created by SezerBozkir<admin@sezerbozkir.com> at 6/29/2020
-# Created by SezerBozkir<admin@sezerbozkir.com> at 6/29/2020
+import sys
+from pathlib import Path
 from publishing_api import HmsPublishingApi
 from pub_env import client_id, client_secret, package_name
 from pprint import pprint
 
 if __name__ == '__main__':
     my_api = HmsPublishingApi(client_id, client_secret, package_name)
-    full_path = r"C:\Users\SezerBozkir\PycharmProjects\sample_path\sample_apk_file.apk"
-    file_name = 'sample_apk_file.apk'
-    file_type = 'apk'  # apk or aab
+    full_path = Path(sys.argv[1])
+    file_name = full_path.name
+    file_type = full_path.suffix  # apk or aab
     app_info = my_api.upload_app(file_path=full_path,
                                  file_name=file_name,
                                  file_type=file_type)
